150c023ec0182235b99793ffa8034458d575c1a8
Qaysi-M <Qaysi_m@outlook.com>
2020-12-30 03:32:47 -0500

    Updates

---
+++ precache-manifest.f4937c9df477e98f9c688569d20f6d06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029ff6b3ba1d43933ae53793f806cadd",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "84265d36defdbb58af03",
+    "url": "/Portfolio/static/css/2.ed890e5e.chunk.css"
+  },
+  {
+    "revision": "84265d36defdbb58af03",
+    "url": "/Portfolio/static/js/2.d233ce07.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/Portfolio/static/js/2.d233ce07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ad37c9ac92c2cfa275c",
+    "url": "/Portfolio/static/js/main.190d0ea4.chunk.js"
+  },
+  {
+    "revision": "4c8b85c1f7e7f3c29fc6",
+    "url": "/Portfolio/static/js/runtime-main.7c70dee3.js"
+  }
+]);
